cc0dc7f5d51723b1784a634e10d1423c6b61cc53
aayush saxena <aayushsaxena15@users.noreply.github.com>
2021-01-15 21:26:50 +0800

    Making changes to correct header in dataframe

--- convert_txt_to_dataframe.py
+++ convert_txt_to_dataframe.py
@@ -1,4 +1,4 @@
 
 import pandas as pd
-data = pd.read_csv('sample_prediction_last_26_rows.txt', sep=",", header=None)
-data.columns = ['row_id', 'predictions']
+data = pd.read_csv('sample_prediction_output.txt', sep=",", header=[0])
+
